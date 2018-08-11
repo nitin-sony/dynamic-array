a4ae38fae07bdd9b92141e52b99717a7c608f9cc
Nitin Soni <nitin.soni@hotmail.co.in>
2018-08-12 03:18:00 +0530

    Complex Pattern

---
+++ Pow.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Pow{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int _new = a + ((int)Math.pow(2,0)*b); 
+            int _old = 0;
+            for(int j=1;j<=n;j++){
+                System.out.print(_new + " ");                
+                _new = _new + ((int)Math.pow(2,j)*b);
+                _old = _new;
+            }
+            System.out.println("");
+        }
+        in.close();
+    }
+}
